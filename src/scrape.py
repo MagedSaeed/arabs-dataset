2969397cbb6d7db518f89750efa25a2d854c531a
MagedSaeed <mageedsaeed1@gmail.com>
2024-10-19 14:05:07 +0300

    add the scraping code

---
+++ src/scrape.py
@@ -0,0 +1,51 @@
+import requests
+from pathlib import Path
+from bs4 import BeautifulSoup
+import re
+
+articles_folder = Path("articles")
+articles_folder.mkdir(parents=True, exist_ok=True)
+
+for i in range(0, 1000):
+    url = f"https://www.asjp.cerist.dz/en/Articles/{i}"
+    volumes_page = requests.get(url).content
+    soup = BeautifulSoup(volumes_page, "html.parser")
+    if not soup.find("aside"):
+        continue
+    # to get the journal title, find the first a element inside the aside element, get its text, and strip leading and trailing whitespace
+    journal_title = soup.find("aside").find("a").text.strip()
+    print("processing journal:", i, "-", journal_title)
+    asjp_folder = articles_folder / f"asjp-{i}-{journal_title}"
+    asjp_folder.mkdir(exist_ok=True)
+    volumes_divs = soup.find("aside").find_all("div")
+    for volume_div in volumes_divs:
+        volume_element = volume_div.find("h2").text.strip()
+        _, volume_number, volume_year = list(filter(None, volume_element.split()))
+        volume_folder = asjp_folder / f"volume-{volume_number}-year-{volume_year}"
+        issues_div = volume_div.find("div")
+        issue_element = issues_div.find("h3").text
+        _, issue_number, issue_date = list(filter(None, issue_element.split()))
+        issue_folder = volume_folder / f"issue-{issue_number}-date-{issue_date}"
+        issue_folder.mkdir(parents=True, exist_ok=True)
+        articles_div = issues_div.find("div")
+        for article_div in articles_div.find_all("h4"):
+            article_element = article_div.find("a")
+            article_title = article_element.text.strip()
+            article_link = article_element["href"]
+            # print(article_title, article_link)
+            article_page = requests.get(article_link).content
+            article_soup = BeautifulSoup(article_page, "html.parser")
+            pdf_link = article_soup.find("button", string="Article en ligne").parent[
+                "href"
+            ]
+            pdf_response = requests.get(pdf_link)
+
+            # Clean the article title by replacing multiple spaces with a single space
+            cleaned_title = re.sub(r"\s+", " ", article_title)
+
+            # Replace any characters that are not allowed in filenames
+            cleaned_title = re.sub(r'[<>:"/\\|?*]', "_", cleaned_title)
+
+            pdf_file = issue_folder / f"{cleaned_title}.pdf"
+            with open(pdf_file, "wb") as f:
+                f.write(pdf_response.content)
