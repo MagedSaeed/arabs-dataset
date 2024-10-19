a0aca58a7373d5ab2d8d9ca34b125f18851ace8c
MagedSaeed <mageedsaeed1@gmail.com>
2024-10-19 14:19:59 +0300

    fix a bug related to long file names

--- src/scrape.py
+++ src/scrape.py
@@ -2,6 +2,22 @@ import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
 import re
+import os
+
+
+def clean_and_truncate_filename(title, max_length=255):
+    # Clean the title by replacing multiple spaces with a single space
+    cleaned_title = re.sub(r"\s+", " ", title.strip())
+
+    # Replace any characters that are not allowed in filenames
+    cleaned_title = re.sub(r'[<>:"/\\|?*]', "_", cleaned_title)
+
+    # Truncate the title if it's too long, preserving the file extension
+    name, ext = os.path.splitext(cleaned_title)
+    if len(name) > max_length - len(ext):
+        name = name[: max_length - len(ext) - 3] + "..."
+    return name + ext
+
 
 articles_folder = Path("articles")
 articles_folder.mkdir(parents=True, exist_ok=True)
@@ -12,10 +28,12 @@ for i in range(0, 1000):
     soup = BeautifulSoup(volumes_page, "html.parser")
     if not soup.find("aside"):
         continue
-    # to get the journal title, find the first a element inside the aside element, get its text, and strip leading and trailing whitespace
+    # Get the journal title
     journal_title = soup.find("aside").find("a").text.strip()
     print("processing journal:", i, "-", journal_title)
-    asjp_folder = articles_folder / f"asjp-{i}-{journal_title}"
+    asjp_folder = articles_folder / clean_and_truncate_filename(
+        f"asjp-{i}-{journal_title}"
+    )
     asjp_folder.mkdir(exist_ok=True)
     volumes_divs = soup.find("aside").find_all("div")
     for volume_div in volumes_divs:
@@ -32,7 +50,6 @@ for i in range(0, 1000):
             article_element = article_div.find("a")
             article_title = article_element.text.strip()
             article_link = article_element["href"]
-            # print(article_title, article_link)
             article_page = requests.get(article_link).content
             article_soup = BeautifulSoup(article_page, "html.parser")
             pdf_link = article_soup.find("button", string="Article en ligne").parent[
@@ -40,11 +57,8 @@ for i in range(0, 1000):
             ]
             pdf_response = requests.get(pdf_link)
 
-            # Clean the article title by replacing multiple spaces with a single space
-            cleaned_title = re.sub(r"\s+", " ", article_title)
-
-            # Replace any characters that are not allowed in filenames
-            cleaned_title = re.sub(r'[<>:"/\\|?*]', "_", cleaned_title)
+            # Clean and truncate the article title
+            cleaned_title = clean_and_truncate_filename(article_title)
 
             pdf_file = issue_folder / f"{cleaned_title}.pdf"
             with open(pdf_file, "wb") as f:
